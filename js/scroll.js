1a556f8569628b3bcefa1307d8fa5aaa4dff6e3a
boboibo26 <lehuyvuok@gmail.com>
2024-09-07 19:58:37 +0700

    upload

---
+++ js/scroll.js
@@ -0,0 +1,7 @@
+// var typed = new Typed(".multiple-text", {
+//     strings: ["Backend Developer",""],
+//     typeSpeed: 500,
+//     backSpeed: 500,
+//     backDelay: 1000,
+//     loop: true
+// });
